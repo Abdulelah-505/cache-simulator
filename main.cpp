f20a948e52e0768c8fa8742f02a68dc83c1ffdcc
Fahad <113018000+1Gwwr@users.noreply.github.com>
2022-10-22 04:28:33 +0300

    add function to convert from binary to hex

--- main.cpp
+++ main.cpp
@@ -336,7 +336,98 @@ void Print(cache_sim cache) {
 }
 
 
+string tag1_prev = "";
+string tag2_prev = "";
 
+
+void createMap(unordered_map<string, char> *um)
+{
+    (*um)["0000"] = '0';
+    (*um)["0001"] = '1';
+    (*um)["0010"] = '2';
+    (*um)["0011"] = '3';
+    (*um)["0100"] = '4';
+    (*um)["0101"] = '5';
+    (*um)["0110"] = '6';
+    (*um)["0111"] = '7';
+    (*um)["1000"] = '8';
+    (*um)["1001"] = '9';
+    (*um)["1010"] = 'a';
+    (*um)["1011"] = 'b';
+    (*um)["1100"] = 'c';
+    (*um)["1101"] = 'd';
+    (*um)["1110"] = 'e';
+    (*um)["1111"] = 'f';
+}
+
+string Binary_To_Hex(string bin) {
+    int BinSize = bin.size();
+    int findDot = bin.find_first_of('.');
+     
+    int len_left = findDot != -1 ? findDot : BinSize;
+     
+    for (int i = 1; i <= (4 - len_left % 4) % 4; i++) {
+        bin = '0' + bin;
+    }
+       
+    if (findDot != -1) {
+        int len_right = BinSize - len_left - 1;
+         
+        for (int i = 1; i <= (4 - len_right % 4) % 4; i++) {
+            bin = bin + '0';
+        }
+    }
+ unordered_map<string, char> bin_hex_map;
+    createMap(&bin_hex_map);
+     
+    int i = 0;
+    string hex = "";
+     
+    while (1) {
+        hex += bin_hex_map[bin.substr(i, 4)];
+        i += 4;
+        if (i == bin.size()) {
+            break;
+        }
+             
+        if (bin.at(i) == '.') {
+            hex += '.';
+            i++;
+        }
+    }
+     
+    // required hexadecimal number
+    return hex;   
+}
+
+void replace(cache_sim &L, string index, string tag, list<Blocks> *tagsList, string line) {
+
+	list<Blocks>::iterator toReplace = tagsList->begin();
+
+	if (replacement == 0) {
+		list<Blocks>::iterator itFind;
+
+		for (itFind = tagsList->begin(); itFind != tagsList->end(); itFind++) { //find LRU and replace
+			if (itFind->LRU_counter < toReplace->LRU_counter) {
+				toReplace = itFind;
+			}
+		}
+
+	}
+
+
+
+
+	Blocks &blk(*toReplace);
+
+	if ((line.find("r") != std::string::npos) || (line.find("R") != std::string::npos))  {
+		blk = Blocks(tag, false); //CORRECT
+	}
+	else if ((line.find("w") != std::string::npos) || (line.find("W") != std::string::npos)) {
+		blk = Blocks(tag, true); //CORRECT
+	}
+
+}
 
 
 
