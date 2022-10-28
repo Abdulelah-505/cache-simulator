56931c8146dbcfd2f13224d19924ec7df7f8795d
Abdulelah <60930802+AbadiHAK@users.noreply.github.com>
2022-10-28 17:22:50 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -9,17 +9,17 @@
 #include "main_memory.h"
 #include "cache_memory.h"
 #include "io.h"
-
-
 using namespace std;
+
+#define CACHE_READ 0
+#define CACHE_WRITE 1 
+
+
+
 
 void run(int, int, int);
 
 int main (int argc, char *argv[ ]){
-	//the values that we get from the 
-	//command line will be put into these
-	//variables. we can use them later in the program
-	//like for checking if they have valid values
 	
 	int cache_capacity = 0;
 	int cache_blocksize = 0;
@@ -38,7 +38,6 @@ int main (int argc, char *argv[ ]){
 	
 	return 0;
 }
-
 void run(int cache_associativity, int cache_blocksize, int cache_capacity){
 	cout << "Allllllllllahhhhhhh Akkkkkkkbrr" << endl;
 	
@@ -53,13 +52,10 @@ void run(int cache_associativity, int cache_blocksize, int cache_capacity){
 	while(!feof(stdin)){
 	  	//read in whether to read or write to the cache
 		cin >> dec >> read_write;
-		
 		// check again if we have reached the end
 		// as this flag is set only after a 'cin'
 		if(feof(stdin)) break;
-		
 		cin >> hex >> address;
-
 		//if it is a cache write, then we have to read the data
 		if(read_write == CACHE_WRITE){
 		  	cin >> hex >> data;
@@ -73,7 +69,6 @@ void run(int cache_associativity, int cache_blocksize, int cache_capacity){
 	// write all dirty lines to memory
 	cache.writeDirtyBlocks();
 	cout << "End of input.\n";
-	
 	cache.print();
 	cout << "Allllllllllahhhhhhh Akkkkkkkbrr" << endl;
 }
